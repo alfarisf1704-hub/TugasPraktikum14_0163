793f51efc4d5f948e90df7d73e29becba7576521
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-06-01 17:18:22 +0700

    membuat void ambil produk

--- tugasPraktikum.cpp
+++ tugasPraktikum.cpp
@@ -5,13 +5,22 @@
 
 using namespace std;
 
+// klass tokoelektronik
 class tokoElektronik
 {
 private:
-    string barangBarang[3];
+    // array
+    string Rak[3];
+    // constructor
     tokoElektronik()
     {
+        Rak[0] = "Kipas Angin";
+        Rak[1] = "Iphone 17 proMax";
+        Rak[2] = "Laptop ROG";
     }
 
 public:
+    void ambilProduk()
+    {
+    }
 }
